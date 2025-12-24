small experimentation. Simplified generator architecture. Corrected gradient bug in SANITY_loss

main/env/env_control_manager.py
@@ -1,5 +1,5 @@
 import torch; import numpy as np; import pylab as plt; from tqdm import tqdm
-from main.utils import tnp; from .env_preprocessing import Env_preprocessing
+from main.utils import tnp; from main.env.env_preprocessing import Env_preprocessing
 from main.model.model_architecture import Model_architecture as Model
 
 class Env_control_manager(Env_preprocessing):
@@ -10,7 +10,6 @@ class Env_control_manager(Env_preprocessing):
         self.controller_training_logs = {'reward': np.zeros((self.reps, self.controller_training_episodes)),
                                          'example_policy': np.zeros((self.reps, self.controller_training_episodes, *self.ctx_dims)),
                                          'prefence_landscape': np.zeros((self.reps, *self.ctx_dims)), 'optimality': np.zeros(self.reps),}
-        
         for self.rep in tqdm(range(reps),  desc = f"{offline_teacher or "ONLINE"}"):
             self.EC_gen_context()
             self.EC_gen_likelihoods()

main/model/model_forward.py
@@ -72,8 +72,5 @@ class Model_forward(Model_backward):
             s = self.sample_to_hid(sample_emb).unsqueeze(1)
             c = self.conf_to_hid(conf).unsqueeze(1)
             z = self.Z_to_pobs(self.active_Z)
-            x1 = s + c + z
-            x2 = self.hid_to_hid(torch.relu(x1)) 
-            x3 = self.hid_to_hid2(torch.relu(x1 + x2))
-            x4 = self.hid_to_pobs(torch.relu(x1 + x2 + x3))
-            self.pred_pobs = torch.sigmoid(x4).squeeze()
+            x = self.hid_to_pobs(torch.relu(s + c + z))
+            self.pred_pobs = torch.sigmoid(x).squeeze()